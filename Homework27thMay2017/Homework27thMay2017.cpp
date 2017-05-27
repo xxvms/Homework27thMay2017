e5b33069b228666b286db684d87e1432ddc9ac79
xxvms <tom@pcservicegroup.co.uk>
2017-05-27 12:23:10 +0100

    testing enviroment

--- Homework27thMay2017/Homework27thMay2017.cpp
+++ Homework27thMay2017/Homework27thMay2017.cpp
@@ -1,11 +1,24 @@
 // Homework27thMay2017.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
+#include <iostream>
+#include <cstdlib>
 
+
+void square(int);
 
 int main()
 {
-    return 0;
+	void(*ptr)(int);
+	ptr = square;
+	ptr(2);      // same as square(2);
+	
+	system("pause");
+	return 0;
 }
-
+void square(int i)
+*A signature of a function is the specification of the number and type of input and output variable eg .
+void square(int i) and
+int add(int i, int j) have different signatures.
+{
+}
